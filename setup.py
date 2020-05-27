1d633c45b018e68fa54de7dba97f87cc806959ed
Aravind Koneru <aravind.b.koneru@gmail.com>
2020-05-26 21:30:28 -0400

    added a license and setup file for pypi

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open('README.md', 'r') as readme:
+    long_description = readme.read()
+
+setuptools.setup(
+    name='walden',
+    version='0.0.1',
+    author='Aravind Koneru',
+    author_email='aravind.b.koneru@gmail.com',
+    description='A .tex journaling system',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/aravindkoneru/Walden',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.5'
+)
