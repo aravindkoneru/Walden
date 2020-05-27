3b05f077460aecd357886146e869d7f7fe4b35e2
Aravind Koneru <aravind.b.koneru@gmail.com>
2020-05-26 20:15:08 -0400

    changed shebang to python3 and hanlded SIGINT

--- journal/edit_journal.py
+++ journal/edit_journal.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 import pathlib
 import datetime
 import subprocess
@@ -14,7 +14,7 @@ def edit_today(journal_path):
         entry_path = pathlib.Path(f'{journal_path}/entries/{today.year}/{today.month}/{today.day}.tex')
     else:
         entry_path = pathlib.Path(f'{journal_path}/entries/{today.year}/0{today.month}/{today.day}.tex')
-    
+
     if not entry_path.is_file():
         create_entry(entry_path)
 
@@ -30,10 +30,10 @@ def create_entry(entry_path):
     entry.append(today.strftime("\\def\\day{\\textit{%B %d, %Y}}"))
     entry.append(today.strftime("\\def\\weekday{\\textit{%A}}"))
     entry.append("\\subsection*{\\weekday, \\day}\n\n")
-    
+
     with open(entry_path, "w") as new_entry:
         new_entry.write('\n'.join(entry))
-    
+
 
 if __name__ == '__main__':
     edit_today('journals/a')
